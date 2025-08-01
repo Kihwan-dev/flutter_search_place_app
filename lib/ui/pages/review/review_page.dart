ed9edb2ee299aa20945e1a11bfc38e2ecc71fdce
Kihwan-dev <rlrkf2420@gmail.com>
2025-08-01 12:37:35 +0900

    ✨ 페이지 & 패키지 추가
    home_page, review_page
    riverpod, geolocator

--- lib/ui/pages/review/review_page.dart
+++ lib/ui/pages/review/review_page.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class ReviewPage extends StatelessWidget {
+  const ReviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text("ReviewPage"),
+    );
+  }
+}
