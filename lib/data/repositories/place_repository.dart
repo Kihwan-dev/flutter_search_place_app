4fe641e832353f9b415120ca843977ce842bbcc4
Kihwan-dev <rlrkf2420@gmail.com>
2025-08-03 02:50:43 +0900

    ✨ searchPlace 메서드 구현

--- lib/data/repositories/place_repository.dart
+++ lib/data/repositories/place_repository.dart
@@ -1,5 +1,25 @@
+import 'dart:convert';
+
+import 'package:flutter_search_place_app/data/models/place.dart';
 import 'package:flutter_search_place_app/data/repositories/base_remote_repository.dart';
 
 class PlaceRepository extends BaseRemoteRepository {
   //
+  Future<List<Place>> searchPlace(String area) async {
+    //
+    final response = await client.get(
+      "",
+      queryParameters: {
+        "query": area,
+        "display": 5,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> map = jsonDecode(response.toString());
+      return List.from(map["items"]).map((e) => Place.fromJson(e)).toList();
+    }
+
+    return [];
+  }
 }
