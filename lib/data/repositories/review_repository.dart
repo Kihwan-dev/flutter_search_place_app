1c3da88d72419a43e2681ce7b6ec25a2775af907
Kihwan-dev <rlrkf2420@gmail.com>
2025-08-03 17:12:43 +0900

    ✨ ReviewRepository에 Firebase에 리뷰 데이터를 추가하는 함수(addReivew()) 구현

--- lib/data/repositories/review_repository.dart
+++ lib/data/repositories/review_repository.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_search_place_app/data/models/place.dart';
 import 'package:flutter_search_place_app/data/models/review.dart';
 
 class ReviewRepository {
-  Future<List<Review>> getReivewsByAddress(Place place) async {
+  Future<List<Review>> getReivewsByAddress({
+    required double mapX,
+    required double mapY,
+  }) async {
     try {
       // 1. 파이어스토어 인스턴스 가지고 오기
       final firestore = FirebaseFirestore.instance;
@@ -25,12 +27,33 @@ class ReviewRepository {
               return Review.fromJson(map);
             },
           )
-          .where((review) => review.mapX == place.mapX && review.mapY == place.mapY)
+          .where((review) => review.mapX == mapX && review.mapY == mapY)
           .toList();
     } catch (e) {
       print("오류 발생");
       print(e);
       return [];
     }
+  }
+
+  Future<bool> addReview({
+    required String content,
+    required double mapX,
+    required double mapY,
+  }) async {
+    // 1. 파이어스토어 인스턴스 가지고 오기
+    final firestore = FirebaseFirestore.instance;
+    // 2. 컬렉션 참조 만들기
+    final collectionRef = firestore.collection("reviews");
+    // 3. 문서 참조 만들기
+    final docRef = collectionRef.doc();
+    // 4. 값 쓰기
+    await docRef.set({
+      "content": content,
+      "mapX": mapX,
+      "mapY": mapY,
+      "createdAt": DateTime.now().toIso8601String(),
+    });
+    return false;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final place = Place(title: "타이틀", category: "카테고리", roadAddress: "주소", mapX: 127.09295783100004, mapY: 37.535002325700546);
+    final place = Place(title: "타이틀", category: "카테고리", roadAddress: "주소", mapX: 127.0929309, mapY: 37.4242296);
     return MaterialApp(
       home: ReviewPage(place),
       // home: HomePage(),
