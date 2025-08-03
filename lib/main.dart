c4f58144989a8828a9bb724c816ab70aeb93077d
Kihwan-dev <rlrkf2420@gmail.com>
2025-08-03 14:46:57 +0900

    ✨ Filrebase 연동을 위한 세팅 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_search_place_app/firebase_options.dart';
 import 'package:flutter_search_place_app/ui/pages/home/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(ProviderScope(child: const MyApp()));
 }
 
